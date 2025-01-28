3c3c7076aedf02cc5d1de00298404da13eb2b61c
rantunovi <56545538+rantunovi@users.noreply.github.com>
2025-01-28 23:53:19 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
 from app import create_app, socketio
+from eventlet import wsgi
+import eventlet
 
 app = create_app()
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  
-    socketio.run(app, host='0.0.0.0', port=port, debug=False) 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    wsgi.server(eventlet.listen(('0.0.0.0', port)), app)
+
